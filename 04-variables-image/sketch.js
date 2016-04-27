194ce0806ceaf18468206a8604584e175f6e008d
Radames Ajna <radamajna@gmail.com>
2016-04-27 20:23:11 +0100

    adding comments

--- 04-variables-image/sketch.js
+++ 04-variables-image/sketch.js
@@ -1,68 +1,89 @@
 'use strict';
-//This is an empty template
 
+//variables for the circle movement
+var cirx; 
+var ciry;
+var ang = 0;
 
-var posx;
-var posy;
+//variables for the bouncing movement 
+var px; 
+var py;
 var incx;
 var incy; 
 
 var catImg;
 
+
+//pre loading image to the server
 function preload() {  // preload() runs once
     catImg = loadImage('cat.png');
 }
 
 function setup() {
-   var myCanvas = createCanvas(500, 500);
+   var myCanvas = createCanvas(windowWidth, windowHeight);
    myCanvas.parent('myCanvas');
   
    //cleaning the background
    background(255,255,255);
   
-   posx = 100;
-   posy = 100;
-   incx = 1;
-   incy = 0;
+
 
    catImg.resize(100,100);
 
-  //frameRate(1);
+   //starting the bouncing movement in a random position
+   px = random(0, width);
+   py = random(0, height);
+    
+   //step amount    
+   incx = 5;
+   incy = 5;
+}
+function draw() {
+    
+  ang = ang + 10; 
+
+    
+  var R = 50; // radius of the rotation
+  cirx = px + R*cos(radians(ang));
+  ciry = py + R*sin(radians(ang));
+ 
+    
+  //update for the liner movements
+  px = px + incx;    
+  py = py + incy;
+
+//make it bounce 
+//checking the boundaries
+ if(px > width){
+     incx = -incx;
+ }
+ if(px < 0){
+     incx = -incx;
+ }
+ 
+ if(py > height){
+     incy = -incy;
+ }
+ if(py < 0 ){
+    incy = -incy; 
+ }
+    
+  background(255,255,255,255);
+  push();
+    translate(cirx, ciry); 
+    rotate(radians(ang/10)); //rotate in relation to the center of the image
+    imageMode(CENTER);
+    image(catImg,0,0);
+  pop();
+   
+  noStroke();
+  fill(255);
+  rect(0,0, 80, 80);
+  fill(0);
+  text(int(px), 10,30);
+  text(int(py), 10,50);
 }
 
-function draw() {
-    //make it bounce 
-
-	posx = posx + incx;
-	posy = posy + incy;
-
-	if(posx > 400){
-	  incx = 0;
-	  incy = 1;
-	  posx = 400;
-	}
-	if(posy < 100){
-	  incx = 1;
-	  incy = 0;
-	  posy = 100;
-	}
-	if(posx < 100){
-	  incx = 0;
-	  incy = -1;
-	  posx = 100;
-	}
-
-	if(posy > 400){
-	  incx = -1;
-	  incy = 0;
-	  posy = 400;
-	}
-
-	
-	background(255,255,255,150);
-	imageMode(CENTER);
-    image(catImg, posx, posy);
-  
-  
-
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
