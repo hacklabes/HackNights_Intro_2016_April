81beeea8d140dab485b055d90747ee7167040ae4
Radamés Ajna <radamajna@gmail.com>
2016-06-01 19:20:25 +0100

    fix error

--- 07-DOM/sketch.js
+++ 07-DOM/sketch.js
@@ -1,13 +1,15 @@
 var canvas, text;
 var sSlider;
-
+var img;
 
 function setup() {
   canvas = createCanvas(600, 400);
   img = createImg("https://www.google.co.uk/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png");
   sSlider = createSlider(0, 255, 100);
   text = createDiv('This is an HTML string!');
+  text.style("color", "green");
   img.position(0,0);
+ 
 }
 
 function draw() {
@@ -15,5 +17,4 @@ function draw() {
   fill(sliderValue);
   ellipse(mouseX,mouseY,100,100);
   img.size(sliderValue,-1);
-  
 }
